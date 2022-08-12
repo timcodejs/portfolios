5eea87c6e939f8c597c803176fc92592dab314f6
Tim <46413981+timcodejs@users.noreply.github.com>
2022-08-12 20:03:47 +0900

    포트폴리오 사이트

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path={process.env.PUBLIC_URL + '/'} element={<Main />} />
       </Routes>
     </Router>
   );
