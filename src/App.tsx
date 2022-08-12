545f037771ed1327f06d21043e53b69c4418fb65
Tim <46413981+timcodejs@users.noreply.github.com>
2022-08-13 02:39:04 +0900

    github pages 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import Main from './pages/main';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Pdf from './pages/pdf';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/resume' element={<Pdf />} />
-      </Routes>
-    </Router>
+    <>
+      <GlobalStyled />
+      <Router basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/resume' element={<Pdf />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+const GlobalStyled = createGlobalStyle`
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
