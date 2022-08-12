28ae25aa590b297645586b8bb1d3ec8bbf969f0f
Tim <46413981+timcodejs@users.noreply.github.com>
2022-08-12 19:56:44 +0900

    포트폴리오 사이트

---
+++ src/pages/main.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const Main = () => {
+  return (
+    <ListStyled>
+      <h2>이태원의 포트폴리오</h2>
+      <Stack spacing={2}>
+        <Button variant='contained'>
+          <Link to='/instaclone/'>인스타그램 클론 코딩</Link>
+        </Button>
+        <Button variant='contained'>
+          <Link to='/movieapp/'>영화 웹 어플리케이션</Link>
+        </Button>
+      </Stack>
+    </ListStyled>
+  );
+};
+
+export default Main;
+
+const ListStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  h2 {
+    text-align: center;
+  }
+  button {
+    width: 600px;
+    height: 50px;
+  }
+  a {
+    display: block;
+    color: #fff;
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    text-decoration: none;
+  }
+`;
